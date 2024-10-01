1f0c97977bc2f2c0627289a783e780dd00d6b0ff
DedeepyaGelli <121683311+DedeepyaGelli@users.noreply.github.com>
2024-10-01 11:28:36 +0530

    allow methods

--- vems/settings.py
+++ vems/settings.py
@@ -62,7 +62,14 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000", 
     "https://your-frontend-domain.com",
 ]
-
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
+]
 ROOT_URLCONF = 'vems.urls'
 
 TEMPLATES = [
